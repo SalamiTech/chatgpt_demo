ee4e936202184e2d39ae2bbf8af6c8628133d5b6
SalamiTech <salamigideon01@gmail.com>
2023-01-13 09:08:34 +0000

    developing

---
+++ lib/chat_message.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({super.key, required this.text, required this.sender});
+
+  final String text;
+  final String sender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
